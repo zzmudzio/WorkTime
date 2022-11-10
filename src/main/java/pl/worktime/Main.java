0424a0af6c1a68559c2a8357497f0f4f75617d58
zzmudzio <zzmudzio@gmail.com>
2022-11-10 09:45:49 +0100

    Main method modifications

--- src/main/java/pl/worktime/Main.java
+++ src/main/java/pl/worktime/Main.java
@@ -11,19 +11,16 @@ public class Main {
             Thread.sleep(3000);
             System.exit(0);
         }
-        if(args.length > 0 && !args[0].equals("install")) {
-            System.out.println("Błąd: podano nieprawidłowe " +
-                    "parametry wejściowe - użyj 'install'" +
-                    "aby zainstalować bazę na serwerze.");
-        }
-        else if(args.length > 0) {
+        if(args.length > 0 && args[0].equals("install")) {
             System.out.println("Aplikacja uruchomiona w trybie instalacyjnym. ");
             DataBaseManager.createDatabase(conn);
         }
-        else {
+        else if(args.length > 0 && args[0].equals("insert")) {
             WorkTimeManager.insertDateTime(conn);
-            System.out.println(WorkTimeManager.displayTodayStartTime(conn));
-            connectionManager.disconnectFromServer(conn);
         }
+        else {
+            System.out.println(WorkTimeManager.displayTodayStartTime(conn));
+        }
+        connectionManager.disconnectFromServer(conn);
     }
 }
