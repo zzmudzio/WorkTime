ec04ce41653b04f54ec70efc34815102075d9328
zzmudzio <zzmudzio@gmail.com>
2022-11-11 19:00:03 +0100

    Added new class SqlServerServices responsible for checking a service availability

--- src/main/java/pl/worktime/Main.java
+++ src/main/java/pl/worktime/Main.java
@@ -4,23 +4,32 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
+        if(!SqlServerServices.areServicesReady()) {
+            System.out.println("Usługi nie są gotowe, przerywam. Sprawdź" +
+                    ",czy odpowiedie usługi SqlServer oraz SqlServer Browser zostały uruchomione");
+            System.exit(2);
+        }
+        /* Creating a sql server connection.. */
         SqlServerConnectionManager connectionManager = SqlServerConnectionManager.getInstance();
-        Connection conn;
-        if((conn = connectionManager.connectToServer()) == null) {
+        Connection connection;
+        if((connection = connectionManager.connectToServer()) == null) {
             System.out.println("Błąd: połączenie nie zostało utworzone, przerywam za 3 sekundy.");
             Thread.sleep(3000);
-            System.exit(0);
+            System.exit(2);
         }
+        /* Checking whether app has been started with a parameters */
         if(args.length > 0 && args[0].equals("install")) {
             System.out.println("Aplikacja uruchomiona w trybie instalacyjnym. ");
-            DataBaseManager.createDatabase(conn);
+            DataBaseManager.createDatabase(connection);
         }
         else if(args.length > 0 && args[0].equals("insert")) {
-            WorkTimeManager.insertDateTime(conn);
+            WorkTimeManager.insertDateTime(connection);
         }
         else {
-            System.out.println(WorkTimeManager.displayTodayStartTime(conn));
+            System.out.println(WorkTimeManager.displayTodayStartTime(connection));
+            Thread.sleep(5000);
         }
-        connectionManager.disconnectFromServer(conn);
+        connectionManager.disconnectFromServer(connection);
     }
 }
---
+++ src/main/java/pl/worktime/SqlServerServices.java
@@ -0,0 +1,75 @@
+package pl.worktime;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+  return codes: -1 = problem during process creation or env path not set correctly;
+  0 = sql server service has not started yet; 1 = sql service has started
+ */
+public class SqlServerServices {
+    private static final String SQL_SRV_NAME_ENV = "ZZMUDZIO_SQL_NAME"; //this env holds a Sql Server service name
+    private static final String SQL_SRV_BROWSER_NAME_ENV = "ZZMUDZIO_SQL_BRW_NAME"; // this env hold a Sql Server browser service name
+    public static boolean checkIfEnvVarsAreSet() {
+        return getServiceName(SQL_SRV_BROWSER_NAME_ENV) != null && getServiceName(SQL_SRV_NAME_ENV) != null;
+    }
+    public static int checkIfServiceIsRunning(String serviceName) {
+        String serviceData;
+        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c","sc query "
+                + getServiceName(serviceName));
+        try {
+            Process myProcess = builder.start();
+            BufferedReader myReader = new BufferedReader(new InputStreamReader(myProcess.getInputStream()));
+            while ((serviceData = myReader.readLine()) != null) {
+                if (serviceData.contains("RUNNING")) {
+                    System.out.println("Usługa " + serviceName + " jest uruchomiona.");
+                    return 1;
+                }
+            }
+            System.out.println("Usługa " + serviceName +" oczekuje na uruchomienie..");
+            return 0;
+        }
+        catch(IOException ioe) {
+            System.out.println("Błąd: wystąpił błąd poczas próby utworzenia procesu");
+            return -1;
+        }
+    }
+    public static String getServiceName(String envName) {
+        return System.getenv(envName);
+    }
+    public static String[] getEnvs() {
+        return new String[] {SQL_SRV_BROWSER_NAME_ENV, SQL_SRV_NAME_ENV};
+    }
+
+    public static boolean areServicesReady() throws InterruptedException {
+        boolean sysEnvVarsStatus = SqlServerServices.checkIfEnvVarsAreSet();
+        if(!sysEnvVarsStatus) {
+            System.out.println("Błąd: Zmienne środowiskowe nie zostały prawidłowo ustawione, przerywam.");
+            return false;
+        }
+        else {
+            String[] sqlServices = SqlServerServices.getEnvs();
+            boolean loopContinue = true;
+            int serviceStatus = 0;
+            for (String service : sqlServices) { //to check whether all desired services are running
+                System.out.println("Oczekiwanie na uruchomienie usługi " + service);
+                while(loopContinue) {
+                    serviceStatus = checkIfServiceIsRunning(service);
+                    switch(serviceStatus) {
+                        case -1:
+                            return false;
+                        case 0:
+                            Thread.sleep(5000);
+                            break;
+                        case 1:
+                            loopContinue = false;
+                    }
+                }
+                loopContinue = true;
+            }
+            return true;
+        }
+    }
+}
+
